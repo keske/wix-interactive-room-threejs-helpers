564829006086de624e58d4ac3a8be273a9d8dade
Andrey Keske <hello@andreykeske.com>
2019-04-21 22:50:17 -0400

    Refactor *

--- src/index.js
+++ src/index.js
@@ -18,96 +18,66 @@ import sphere from './lib/sphere';
  * Demo
  */
 R.pipe(
-  () => (
-    document.getElementById('root')
-  ),
-  (root) => {
+  () => ({
+    generateAnimateProps: () => ({
+      animate: {
+        position: {
+          x: Math.floor(Math.random() * 200),
+          y: Math.floor(Math.random() * 200),
+          z: Math.floor(Math.random() * 20),
+        },
+        rotation: {
+          x: Math.floor(Math.random() * 360),
+          y: Math.floor(Math.random() * 360),
+          z: Math.floor(Math.random() * 360),
+        },
+      },
+    }),
+    root: document.getElementById('root'),
+  }),
+  ({ generateAnimateProps, root }) => {
     ReactDOM.render((
       R.pipe(
         ({ objects, styles }) => (
           <div style={styles.root}>
             {
               R.not(R.isEmpty(objects)) && (
-                <div style={styles.scene}>
-                  <Scene
-                    {...{
-                      acceleration: {
-                        x: 0,
-                        y: 0,
-                        z: 0,
-                      },
-                      mouse: {
-                        x: 0,
-                        y: 0,
-                      },
-                      objects,
-                      screen: {
-                        width: window.innerWidth,
-                        height: window.innerHeight,
-                      },
-                    }}
-                  >
-                    {
-                      (/* { scene } */) => false
-                    }
-                  </Scene>
-                </div>
+                <Scene
+                  {...{
+                    acceleration: {
+                      x: 0,
+                      y: 0,
+                      z: 0,
+                    },
+                    mouse: {
+                      x: 0,
+                      y: 0,
+                    },
+                    objects,
+                    screen: {
+                      width: window.innerWidth,
+                      height: window.innerHeight,
+                    },
+                  }}
+                >
+                  {
+                    (/* { scene } */) => false
+                  }
+                </Scene>
               )
             }
           </div>
         ),
       )({
         objects: [{
-          animate: {
-            position: {
-              x: 50,
-              y: 50,
-              z: 20,
-            },
-            rotation: {
-              x: 10,
-              y: 10,
-              z: 10,
-            },
-          },
+          ...generateAnimateProps(),
           object: sphere(),
-          render: {
-            type: 'sphere',
-          },
         }, {
-          animate: {
-            position: {
-              x: 100,
-              y: 100,
-              z: 10,
-            },
-            rotation: {
-              x: 10,
-              y: 10,
-              z: 10,
-            },
-          },
+          ...generateAnimateProps(),
           object: cube(),
-          render: {
-            type: 'cube',
-          },
         }, {
-          animate: {
-            position: {
-              x: 70,
-              y: 70,
-              z: 20,
-            },
-            rotation: {
-              x: 10,
-              y: 10,
-              z: 10,
-            },
-          },
+          ...generateAnimateProps(),
           object: lens(),
-          render: {
-            type: 'lens',
-          },
         }],
         styles: {
           root: {
@@ -117,10 +87,6 @@ R.pipe(
             backgroundColor: 'black',
             width: window.innerWidth,
             height: window.innerHeight,
-          },
-          scene: {
-            position: 'absolute',
-            zIndex: 10,
           },
         },
       })
