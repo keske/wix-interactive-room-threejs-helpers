81a8719c73b0495b09614cc2d0394243f772877e
Andrey Keske <hello@andreykeske.com>
2019-04-28 18:38:04 -0400

    Add Octahedron

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import circle from './lib/circle';
 import cone from './lib/cone';
 import cube from './lib/cube';
 import dodecahedron from './lib/dodecahedron';
+import octahedron from './lib/octahedron';
 import sphere from './lib/sphere';
 import tetrahedron from './lib/tetrahedron';
 import torus from './lib/torus';
@@ -68,6 +69,9 @@ R.pipe(
         }, {
           ...generateAnimateProps(),
           object: dodecahedron(),
+        }, {
+          ...generateAnimateProps(),
+          object: octahedron(),
         }, {
           ...generateAnimateProps(),
           object: sphere(),
---
+++ src/lib/octahedron.js
@@ -0,0 +1,44 @@
+// @flow
+
+import * as THREE from 'three';
+
+// Materails
+import colorMaterial from './colorMaterial';
+import refractionMatarial from './refractionMatarial';
+
+type Props = {
+  color: string,
+  detail?: number,
+  images?: Array<string>,
+  path?: string,
+  radius: number,
+  refraction?: boolean,
+  refractionRatio?: number,
+};
+
+export default ({
+  color = '#FFF',
+  detail = 0,
+  images = [
+    'back.png',
+    'back.png',
+    'left.png',
+    'right.png',
+    'top.png',
+    'bottom.png',
+  ],
+  path = 'http://localhost:3030/cube/',
+  radius = 10,
+  refraction = true,
+  refractionRatio = 0.95,
+}: Props = {}): * => (
+  new THREE.Mesh(
+    new THREE.OctahedronGeometry(
+      radius,
+      detail,
+    ),
+    refraction
+      ? refractionMatarial({ images, path, refractionRatio })
+      : colorMaterial({ color }),
+  )
+);
