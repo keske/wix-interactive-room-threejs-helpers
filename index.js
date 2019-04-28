fc691eeb69ef69fd72748ea94d3bf0903df5b98c
Andrey Keske <hello@andreykeske.com>
2019-04-27 20:51:05 -0400

    Import/Export

---
+++ index.js
@@ -0,0 +1,15 @@
+import Scene from './lib/Scene';
+
+import cube from './lib/cube';
+import sphere from './lib/sphere';
+import tetrahedron from './lib/tetrahedron';
+import torus from './lib/torus';
+
+export default Scene;
+
+export {
+  cube,
+  sphere,
+  tetrahedron,
+  torus,
+};
--- src/index.js
+++ src/index.js
@@ -15,15 +15,6 @@ import sphere from './lib/sphere';
 import tetrahedron from './lib/tetrahedron';
 import torus from './lib/torus';
 
-export default Scene;
-
-export {
-  cube,
-  sphere,
-  tetrahedron,
-  torus,
-};
-
 /**
  * Demo
  */
